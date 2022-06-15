1d92a49a94acdf82c19ae022f6dba0041384f78c
aselekoglu <aselekoglu97@gmail.com>
2022-06-15 13:34:48 +0300

    'copy as HTML' button added

--- ccs-signature-generator/js/script.js
+++ ccs-signature-generator/js/script.js
@@ -3,42 +3,60 @@ let clipboard = new ClipboardJS(".btn");
 var searchBlock = document.getElementById('output');
 var links = document.getElementsByClassName('sc-hzDkRC')
 
+// console.log(URLSearchParams);
+
 function generateSig() {
 
-    var name = document.getElementById('full-name').value,
-        title = document.getElementById('title').value,
-        email = document.getElementById('email').value,
-        phone = document.getElementById('phone').value,
-        photo_id = document.getElementById('photo-id').value;
+    var signature = {
+        name: document.getElementById('full-name').value,
+        title: document.getElementById('title').value,
+        email: document.getElementById('email').value,
+        phone: document.getElementById('phone').value,
+        photo_id: document.getElementById('photo-id').value
+    }
 
-    console.log(name, title, email, phone, photo_id, searchBlock);
 
-    $(searchBlock).find('#sign-name').text(name);
-    $(searchBlock).find('#sign-title').text(title);
-    $(searchBlock).find('#sign-email-vis').text(email);
-    $(searchBlock).find('#sign-email-href').attr('href', 'mailto:' + email);
-    $(searchBlock).find('#sign-mobile-vis').text(phone);
-    $(searchBlock).find('#sign-mobile-href').attr('href', 'tel:' + phone);
-    $(searchBlock).find('#photo').attr('src', 'https://drive.google.com/uc?id=' + photo_id);
+    // console.log(name, title, email, phone, photo_id, searchBlock);
+
+    $(searchBlock).find('#sign-name').text(signature.name);
+    $(searchBlock).find('#sign-title').text(signature.title);
+    $(searchBlock).find('#sign-email-vis').text(signature.email);
+    $(searchBlock).find('#sign-email-href').attr('href', 'mailto:' + signature.email);
+    $(searchBlock).find('#sign-mobile-vis').text(signature.phone);
+    $(searchBlock).find('#sign-mobile-href').attr('href', 'tel:' + signature.phone);
+    $(searchBlock).find('#photo').attr('src', 'https://drive.google.com/uc?id=' + signature.photo_id);
 
     $(links).each(function () {
         var $this = $(this);
         var _href = $this.attr("href");
-        $this.attr("href", _href + email)
+        $this.attr("href", _href + signature.email)
     })
 
-    console.log($(links).attr("href"))
+    // console.log($(links).attr("href"))
 
     $('button').show();
 }
 
 clipboard.on('success', function (e) {
-    $('#js-copy').val('Successfully copied!');
+    console.log(e)
+    // console.log(document.getElementById("code"))
+    var id = "";
+    id = id.concat("#", e.trigger.attributes[0].nodeValue.toString())
+    $(id).val('Successfully copied!');
 });
+
 
 clipboard.on('error', function (e) {
-    $('#js-copy').val('Could not be copied. See console for the error log.');
-    console.log(e);
+    var id = "";
+    id = id.concat("#", e.trigger.attributes[0].nodeValue.toString())
+    $(id).val('Could not be copied. See console for the error log.');
+    // console.log(e);
 });
 
+function copyHTML() {
+    var outerHTML = document.getElementById('code').outerHTML;
+    // console.log(outerHTML)
+    navigator.clipboard.writeText(outerHTML);
+    $('#hs-copy').val('Successfully copied!');
+}
 
